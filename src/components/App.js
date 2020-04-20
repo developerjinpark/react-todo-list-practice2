ae63e9bc148ee21fa57f41db6c8585c4982e88fb
Jin <jinpark1504@gmail.com>
2020-04-20 19:55:45 -0400

    apply redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,90 @@
 import React, { Component } from 'react';
 import PageTemplate from 'components/PageTemplate';
-import TodoInput from 'components/TodoInput';
-import TodoList from 'components/TodoList';
+// import TodoInput from 'components/TodoInput';
+// import TodoList from 'components/TodoList';
+import TodoInputContainer from 'containers/TodoInputContainer';
+import TodoListContainer from 'containers/TodoListContainer';
 
-const initialTodos = new Array(500).fill(0).map(
-  (foo, index) => ({id: index, text: `Todo ${index}`, done: false})
-);
+// const initialTodos = new Array(500).fill(0).map(
+//   (foo, index) => ({id: index, text: `Todo ${index}`, done: false})
+// );
 
 class App extends Component {
-  state = {
-    input:'',
-    // todos: [
-    //   { id: 0, text: 'Study React', done: true },
-    //   { id: 1, text: 'Study English', done: false },
-    // ]
-    todos: initialTodos
-  }
+  // state = {
+  //   input:'',
+  //   // todos: [
+  //   //   { id: 0, text: 'Study React', done: true },
+  //   //   { id: 1, text: 'Study English', done: false },
+  //   // ]
+  //   todos: initialTodos
+  // }
 
-  id = 1;
-  getId = () => {
-    return ++this.id;
-  }
+  // id = 1;
+  // getId = () => {
+  //   return ++this.id;
+  // }
 
-  handleChange = e => {
-    const { value } = e.target;
-    this.setState({
-      input: value
-    });
-  }
+  // handleChange = e => {
+  //   const { value } = e.target;
+  //   this.setState({
+  //     input: value
+  //   });
+  // }
 
-  handleInsert = () => {
-    const { todos, input } = this.state;
+  // handleInsert = () => {
+  //   const { todos, input } = this.state;
 
-    const newTodo = {
-      text: input,
-      done: false,
-      id: this.getId()
-    }
+  //   const newTodo = {
+  //     text: input,
+  //     done: false,
+  //     id: this.getId()
+  //   }
 
-    this.setState({
-      todos: [ ...todos, newTodo ],
-      input: ''
-    });
-  }
+  //   this.setState({
+  //     todos: [ ...todos, newTodo ],
+  //     input: ''
+  //   });
+  // }
 
-  handleToggle = id => {
-    const { todos } = this.state;
-    const index = todos.findIndex(todo => todo.id === id);
+  // handleToggle = id => {
+  //   const { todos } = this.state;
+  //   const index = todos.findIndex(todo => todo.id === id);
 
-    const toggled = {
-      ...todos[index],
-      done: !todos[index].done
-    };
+  //   const toggled = {
+  //     ...todos[index],
+  //     done: !todos[index].done
+  //   };
 
-    this.setState({
-      todos: [
-        ...todos.slice(0, index),
-        toggled,
-        ...todos.slice(index + 1, todos.length)
-      ]
-    })
-  }
+  //   this.setState({
+  //     todos: [
+  //       ...todos.slice(0, index),
+  //       toggled,
+  //       ...todos.slice(index + 1, todos.length)
+  //     ]
+  //   })
+  // }
 
-  handleRemove = id => {
-    const { todos } = this.state;
-    const index = todos.findIndex(todo => todo.id === id);
+  // handleRemove = id => {
+  //   const { todos } = this.state;
+  //   const index = todos.findIndex(todo => todo.id === id);
 
-    this.setState({
-      todos: [
-        ...todos.slice(0, index),
-        ...todos.slice(index + 1, todos.length)
-      ]
-    })
-  }
+  //   this.setState({
+  //     todos: [
+  //       ...todos.slice(0, index),
+  //       ...todos.slice(index + 1, todos.length)
+  //     ]
+  //   })
+  // }
   render() {
-    const { input, todos } = this.state;
-    const { handleChange, handleInsert, handleToggle, handleRemove } = this;
+    // const { input, todos } = this.state;
+    // const { handleChange, handleInsert, handleToggle, handleRemove } = this;
 
     return (
       <PageTemplate>
-        <TodoInput onChange={handleChange} value={input} onInsert={handleInsert} />
-        <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
+        {/* <TodoInput onChange={handleChange} value={input} onInsert={handleInsert} />
+        <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove} /> */}
+        <TodoInputContainer />
+        <TodoListContainer />
       </PageTemplate>
     );
   }
---
+++ src/modules/index.js
@@ -0,0 +1,5 @@
+import input from './input';
+import todos from './todos';
+import { combineReducers } from 'redux';
+
+export default combineReducers({ input, todos });
